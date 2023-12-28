334aaca24d3599ca4e76fa745c5550e2067c2c39
Elkeid-me <elkeid.me@gmail.com>
2023-12-28 12:57:09 +0800

    `router` 类

--- router.cxx
+++ router.cxx
@@ -1,4 +1,29 @@
 #include "router.hxx"
+#include <bit>
+#include <cstdint>
+
+template <typename T>
+    requires(sizeof(T) == sizeof(std::uint32_t))
+T byte_swap(T x)
+{
+    return __builtin_bswap32(static_cast<std::uint32_t>(x));
+}
+
+std::uint32_t header::get_src() const { return byte_swap(src); }
+std::uint32_t header::get_dst() const { return byte_swap(dst); }
+header_type header::get_type() const { return type; }
+std::uint16_t header::get_length() const { return length; }
+void header::make_header(std::uint32_t _src, std::uint32_t _dst, header_type _type,
+                         std::uint16_t _length)
+{
+    src = byte_swap(_src);
+    dst = byte_swap(_dst);
+    type = _type;
+    length = _length;
+}
+
+static_assert(std::endian::native == std::endian::little);
+static_assert(sizeof(header) == 12);
 
 RouterBase *create_router_object() { return new Router; }
 
@@ -8,4 +33,8 @@ void Router::router_init(int port_num, int external_port, char *external_addr,
     return;
 }
 
-int Router::router(int in_port, char *packet) { return 1; }
+int Router::router(int in_port, char *packet)
+{
+    const header *header_ptr{reinterpret_cast<header *>(packet)};
+    return 0;
+}
--- router.hxx
+++ router.hxx
@@ -1,4 +1,39 @@
 #include "router_prototype.hxx"
+#include <cstdint>
+
+enum class header_type : std::uint8_t
+{
+    dv = 0x00,
+    data = 0x01,
+    control = 0x02
+};
+
+enum class command_type
+{
+    trigger_dv_send = 0,
+    release_nat_term = 1,
+    port_value_change = 2,
+    add_host = 3,
+    block_addr = 5,
+    unblock_addr = 6
+};
+
+class header
+{
+private:
+    std::uint32_t src;
+    std::uint32_t dst;
+    header_type type;
+    std::uint16_t length;
+
+public:
+    std::uint32_t get_src() const;
+    std::uint32_t get_dst() const;
+    header_type get_type() const;
+    std::uint16_t get_length() const;
+
+    void make_header(std::uint32_t, std::uint32_t, header_type, std::uint16_t);
+};
 
 class Router : public RouterBase
 {
